94213acfa0362c08b9a9dcc4ab8b75d08caab934
Johannes Beisswenger <johannes@beisswenger.name>
2023-08-17 16:17:56 +0200

    [WIP] main: simplified sleep routine (borked with GPIO?)

--- zbs243_clock_Tag_FW/main.c
+++ zbs243_clock_Tag_FW/main.c
@@ -70,6 +70,68 @@ void test_sleep_power_consumption() {
 
 }
 
+void timed_sleep1(uint32_t length)
+{
+	__bit irqEn = IEN_EA; // save previous IRQ state
+	uint8_t prescaler;
+	IEN_EA = 0; // IRQs off
+	
+	if (!length)
+		length = 0xfffffffful;
+
+        // is this a cheap variant of 	powerDown(INIT_RADIO); ? it matches except for the radioRxEnable(false,true);
+        // since IRQs are off this is 100% equivalent... except a few instructions shorter...
+	RADIO_IRQ4_pending = 0;
+	UNK_C1 &=~ 0x81;
+	TCON &=~ 0x20; // clear T= overflow bit
+	RADIO_command = RADIO_CMD_UNK_2;
+	RADIO_command = RADIO_CMD_UNK_3;
+        //powerDown(INIT_RADIO); // this would be slower, but equivalent and actually less code...
+
+        // sleep timer seems to count down?
+	if (length <= 0x00008000ul) {
+		length <<= 5;
+		prescaler = 0x56;		//0x56 = one tick is 1/32k of sec
+	}
+	else {
+		if (length != 0xfffffffful)
+			length += 500;
+		length /= 1000;
+		prescaler = 0x16;		//0x16 = one tick is 1 second
+	}
+	if (length > 0x000fffff) {
+		RADIO_SleepTimerLo = 0xff;
+		RADIO_SleepTimerMid = 0xff;
+		RADIO_SleepTimerHi = 0x0f;
+	}
+	else {
+		RADIO_SleepTimerLo = length;
+		RADIO_SleepTimerMid = length >> 8;
+		RADIO_SleepTimerHi = ((uint8_t)(length >> 16)) & 0x0f;
+	}
+
+	__asm__("nop");
+	RADIO_SleepTimerSettings = prescaler;
+	__asm__("nop\nnop\nnop\nnop\n");
+	RADIO_SleepTimerSettings |= 0x80;
+	__asm__("nop\nnop\n");
+	RADIO_RadioPowerCtl = 0x44;
+	__asm__("nop\nnop\n");
+
+        // Why do we need this busy sleep here?!
+	//make sure time does not run backwards
+	/* TL0 = 0x0; */
+	/* TH0 = 0xFF; */
+	/* while (TH0 == 0xFF); // so this busily waits for 8bits of count on timer 0?! */
+
+        // this is the reverse of the clearing of these flags above, identical to the powerDown(INIT_RADIO)?
+        // but it is not equivalent to powerUp(INIT_RADIO)
+	//UNK_C1 |= 0x81;
+	//TCON |= TCON_TF0_MASK; /* FIXME: why would we write to it? isnt this a read only value? also why dont we use bit addressing? this is cleared by some ISRs so maybe we want to detect that..?*/
+        // maybe Timer 0 does not signal overflow if IRQs are off, and this is needed to emulate the behavior of Timer 0 overflowing
+
+	IEN_EA = irqEn;
+}
 
 #define GPIO_IN 1
 #define GPIO_OUT 0
